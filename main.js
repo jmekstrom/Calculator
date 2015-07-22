db3fddbce3a1f6ea5476ca5baef2e51f5816c6dd
Jason Ekstrom <JEkstroms@Jasons-MacBook-Pro.local>
2015-07-22 11:47:35 -0700

    calculator 0.1 and beyond

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var display_val = "";
 var new_operand = "";
-var operands = [];
+var equation_array = [];
+var answer = "";
 
 function input_digit(n) {
     display_val = display_val + n;
@@ -11,21 +12,24 @@ function input_digit(n) {
 
 function operator(x){
 	display_val = display_val + x;
-	operands.push(new_operand);
+	var new_operator = x; 
+	equation_array.push(new_operand);
 	new_operand = '';
+	equation_array.push(new_operator);
+	new_operator = '';
 	update_display();
 }
 
 function update_display(){
 	$("#display_input").val(display_val);
 	console.log("Display value is:",display_val);
-	console.log("Input Array:",operands);
+	console.log("equation Array is:",equation_array);
 }
 
 function AC() {
     $("#display_input").val("");
     display_val = "";
-    operands = [];
+    equation_array = [];
     console.log("input display cleared")
 }
 
@@ -37,7 +41,37 @@ function C() {
 
 function equals(e) {
 	$("#display_history").val(display_val);
-	var answer = eval(display_val);
-	display_val = answer;
+	equation_array.push(new_operand);
+	new_operand = '';
+	calculate();
+	display_val = e + answer;
 	update_display(); 
+}
+
+function calculate() {
+	//// sort values to calculate
+	var operand1 = '';
+	var operator = '';
+	var operand2 = '';
+	for (var i = 0; i < equation_array.length; i+=3) {
+		operand1 = equation_array[i];
+		operator = equation_array[i+1];
+		operand2 = equation_array[i+2];
+	};
+	console.log(operand1,operator,operand2);
+	switch(operator){
+		case "+":
+			answer = parseFloat(operand1) + parseFloat(operand2);
+			break;
+		case "-":
+			answer = parseFloat(operand1) - parseFloat(operand2);
+			break;
+		case "x":
+			answer = parseFloat(operand1) * parseFloat(operand2);
+			break;
+		case "/":
+			answer = parseFloat(operand1) / parseFloat(operand2);
+			break;
+	}
+
 }
